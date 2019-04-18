40a30c92752d1629a01d403f54960e1981d335fb
JLuizCardoso <julia.luizcardoso@bol.com.br>
2019-04-17 21:56:16 -0300

    Começo FrontEnd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nome = 'Julia';
+
+
+
+  categorias = [
+    {id: "1", nome: "Categorias 1"},
+    {id: "2", nome: "Categorias 2"},
+    {id: "3", nome: "Categorias 3"},
+    {id: "4", nome: "Categorias 4"},
+    {id: "5", nome: "Categorias 5"},
+    {id: "6", nome: "Categorias 6"},
+    {id: "7", nome: "Categorias 7"},
+    {id: "8", nome: "Categorias 8"},
+    {id: "9", nome: "Categorias 9"},
+    {id: "10", nome: "Categorias 10"},
+
+  ];
 }
+
